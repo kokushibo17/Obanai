791c65592e0fddd38812a14b7e429021fd2b2274
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-22 14:12:34 +0530

    Create __main__.py

---
+++ breathing/__main__.py
@@ -0,0 +1,9 @@
+#import uvloop  # Comment it out if using on windows
+
+from Powers.bot_class import Obanai
+
+if __name__ == "__main__":
+
+    #uvloop.install() # Comment it out if using on windows
+
+    Obanai().run()
