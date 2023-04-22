4aeddbb03d78573fa9f35a83a45512c6020aaeb2
borusara <125817831+borusara@users.noreply.github.com>
2023-04-22 11:29:59 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -26,4 +26,4 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it
-nssm restart @ObanaihashiraBot
+nssm restart ObanaihashiraBot
