eb0aae64b2ea98398412879f71ee9ea1867bf113
borusara <125817831+borusara@users.noreply.github.com>
2023-06-14 12:25:08 +0530

    Add files via upload

---
+++ breathing/modules/mongo/chatbot_mongo.py
@@ -0,0 +1,25 @@
+
+from Messi.mongo import db
+
+chatbotdb = db.chatbot
+
+def is_chatbot(chat_id):
+    chatbot = chatbotdb.find_one({"chat_id": chat_id})
+    if not chatbot:
+        return False
+    else:
+        return True
+
+def add_chatbot(chat_id):
+    chatbot = is_chatbot(chat_id)
+    if chatbot:
+        return
+    else:
+        return chatbotdb.insert_one({"chat_id": chat_id})
+
+def rm_chatbot(chat_id):
+    chatbot = is_chatbot(chat_id)
+    if not chatbot:
+        return
+    else:
+        return chatbotdb.delete_one({"chat_id": chat_id})
---
+++ breathing/modules/mongo/karma_mongo.py
@@ -0,0 +1,24 @@
+from Messi.mongo import db
+
+karma_statusdb = db.karma_status
+
+def is_karma(chat_id):
+    karma = karma_statusdb.find_one({"chat_id": chat_id})
+    if not karma:
+        return False
+    else:
+        return True
+
+def set_karma(chat_id):
+    karma = is_karma(chat_id)
+    if karma:
+        return
+    else:
+        return karma_statusdb.insert_one({"chat_id": chat_id})
+
+def rem_karma(chat_id):
+    karma = is_karma(chat_id)
+    if not karma:
+        return
+    else:
+        return karma_statusdb.delete_one({"chat_id": chat_id})
